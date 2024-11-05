d1b0f0a2e919dbda37bf640aaa20f97f2ab8dcf8
junheeLee <junhee2727@gmail.com>
2024-11-05 23:43:21 +0900

    etc

--- src/components/units/index/Index.container.js
+++ src/components/units/index/Index.container.js
@@ -1,17 +1,55 @@
 import IndexUI from "./Index.presenter";
+import { useState, useEffect } from "react";
 
 export default function IndexLogic() {
   //JS Section
+  const [file, setFile] = useState(null);
 
-  const onClickUpload = (event) => {
-    const fileInput = event.target.nextElementSibling;
-    const file = fileInput.files;
-    const formData = new FormData();
-    formData.append("file", file);
-
-    console.log(formData);
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    //예외처리
+    if(!file){
+      alert("파일이 입력되지 않았습니다.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file',file);
+
+  // 서버 요청
+  //   try {
+  //     const response = await fetch('/upload', {
+  //         method: 'POST',
+  //         body: formData
+  //     });
+
+  //     if (response.ok) {
+  //         const result = await response.json();
+  //         console.log('Upload Success:', result);
+  //         alert('File uploaded successfully');
+  //     } else {
+  //         console.error('Upload Failed:', response.statusText);
+  //         alert('File upload failed');
+  //     }
+  // } catch (error) {
+  //     console.error('Error:', error);
+  //     alert('An error occurred while uploading the file');
+  // }
+  
+  }
+  
+  //File이 변경될 때 호출, file console.log 로 띄우기
+  // useEffect(() => {
+  //   if(file){
+  //     console.log("File selected: ", file);
+  //   }
+  // }, [file]);
+
   //HTML Section
-  return <IndexUI aaa={onClickUpload}></IndexUI>;
+  return <IndexUI hs={handleSubmit} hfc = {handleFileChange}></IndexUI>;
 }
--- src/components/units/index/Index.presenter.js
+++ src/components/units/index/Index.presenter.js
@@ -11,10 +11,12 @@ export default function IndexUI(props) {
   //HTML Section
   return (
     <Index__Wrapper>
-      <Index__FileInput as="label" htmlFor="input-file" onClick={props.aaa}>
+      <form onSubmit={props.hs}>
+      <Index__FileInput as="label" htmlFor="input-file">
         Input .run File Here!
       </Index__FileInput>
-      <Index__FileHidden type="file" id="input-file"></Index__FileHidden>
+      <Index__FileHidden type="file" accept=".json" id="input-file" onChange={props.hfc}></Index__FileHidden>
+      </form>
       <Index__Title_Stat>Statistics Online</Index__Title_Stat>
       <Index__Statistics__container>
         <Index__Statistics__element__IC></Index__Statistics__element__IC>
