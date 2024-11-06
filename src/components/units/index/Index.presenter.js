cca9670f117f29984b6a6f9d1901f576a1a4c5e2
Alonier_mac <junhee2727@gmail.com>
2024-11-06 17:50:15 +0900

    devloping Main Page

--- src/components/units/index/Index.presenter.js
+++ src/components/units/index/Index.presenter.js
@@ -2,13 +2,48 @@ import {
   Index__FileHidden,
   Index__FileInput,
   Index__Form,
+  Index__Table,
   Index__Statistics__container,
-  Index__Statistics__element__IC,
+  Index__Statistics__element,
   Index__Title_Stat,
   Index__Wrapper,
+  Index__Element__Subtitle,
+  Index__Element_BtnContainer,
+  Index__StyledButton,
 } from "./Index.styles";
 
+import { Table } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text) => <b>{text}</b>,
+  },
+  {
+    title: "Rate",
+    dataIndex: "rate",
+    key: "rate",
+  },
+];
+
+const Data = [
+  { key: "1", name: "Strike", rate: 100.0 },
+  { key: "2", name: "Streamline", rate: 24.3 },
+  { key: "3", name: "Sweeping Beam", rate: 22.1 },
+  { key: "4", name: "Cold Snap", rate: 21.1 },
+  { key: "5", name: "Go for the Eyes", rate: 18.1 },
+  { key: "6", name: "Rebound", rate: 14.1 },
+  { key: "7", name: "Beam Cell", rate: 12.1 },
+  { key: "8", name: "Ball Lightning", rate: 10.1 },
+];
+
 export default function IndexUI(props) {
+  //JS Section
+  //서버 데이터 로딩 (임시로 배열 사용)
+  //구현 필요
+
   //HTML Section
   return (
     <Index__Wrapper>
@@ -25,10 +60,63 @@ export default function IndexUI(props) {
       </Index__Form>
       <Index__Title_Stat>Statistics Online</Index__Title_Stat>
       <Index__Statistics__container>
-        <Index__Statistics__element__IC></Index__Statistics__element__IC>
-        <Index__Statistics__element__IC></Index__Statistics__element__IC>
-        <Index__Statistics__element__IC></Index__Statistics__element__IC>
-        <Index__Statistics__element__IC></Index__Statistics__element__IC>
+        <Index__Statistics__element>
+          <Index__Element__Subtitle>Card PickRate</Index__Element__Subtitle>
+          <Index__Element_BtnContainer>
+            <Index__StyledButton
+              style={{ backgroundColor: "#831317", borderColor: "#831317" }}
+            >
+              IronClad
+            </Index__StyledButton>
+            <Index__StyledButton
+              style={{ backgroundColor: "#3B820E", borderColor: "#3B820E" }}
+            >
+              Silent
+            </Index__StyledButton>
+            <Index__StyledButton
+              style={{ backgroundColor: "#13557E", borderColor: "#13557E" }}
+            >
+              Defect
+            </Index__StyledButton>
+            <Index__StyledButton
+              style={{ backgroundColor: "#5E3A77", borderColor: "#5E3A77" }}
+            >
+              Watcher
+            </Index__StyledButton>
+          </Index__Element_BtnContainer>
+          <Index__Element_BtnContainer>
+            <Index__StyledButton
+              style={{ backgroundColor: "#99CDF2", borderColor: "#99CDF2" }}
+            >
+              Floor 1
+            </Index__StyledButton>
+            <Index__StyledButton
+              style={{ backgroundColor: "#99CDF2", borderColor: "#99CDF2" }}
+            >
+              Floor 2
+            </Index__StyledButton>
+            <Index__StyledButton
+              style={{ backgroundColor: "#99CDF2", borderColor: "#99CDF2" }}
+            >
+              Floor 3
+            </Index__StyledButton>
+          </Index__Element_BtnContainer>
+          <Index__Table
+            columns={columns}
+            dataSource={Data}
+            pagination={false}
+            rowHoverable={false}
+          ></Index__Table>
+        </Index__Statistics__element>
+        <Index__Statistics__element>
+          <Index__Element__Subtitle>???</Index__Element__Subtitle>
+          <Index__Table
+            columns={columns}
+            dataSource={Data}
+            pagination={false}
+            rowHoverable={false}
+          ></Index__Table>
+        </Index__Statistics__element>
       </Index__Statistics__container>
     </Index__Wrapper>
   );
--- src/components/units/index/Index.styles.js
+++ src/components/units/index/Index.styles.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Button, Table } from "antd";
 
 export const Index__Wrapper = styled.div`
   display: flex;
@@ -52,9 +53,52 @@ export const Index__Statistics__container = styled.div`
   gap: 20px;
 `;
 
-export const Index__Statistics__element__IC = styled.div`
+export const Index__Statistics__element = styled.div`
   width: 400px;
-  height: 900px;
+  padding: 10px;
 
-  background-color: red;
+  background-color: #354480;
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+export const Index__Element__Subtitle = styled.div`
+  color: white;
+  font-size: 28px;
+  width: 100%;
+  text-align: center;
+`;
+
+export const Index__Element_BtnContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Index__StyledButton = styled(Button)``;
+
+export const Index__Table = styled(Table)`
+  .ant-table {
+    background-color: transparent !important;
+    border: 1px solid black;
+  }
+  .ant-table-thead > tr > th {
+    background-color: transparent !important;
+    border-radius: 0 !important;
+    border-bottom: 2px solid black;
+  }
+  .ant-table-tbody > tr > td {
+    background-color: transparent;
+    border-bottom: 1px solid black;
+  }
+
+  .ant-table-thead th.ant-table-cell::before {
+    content: "";
+    width: 0px;
+    height: 0% !important;
+  }
 `;
